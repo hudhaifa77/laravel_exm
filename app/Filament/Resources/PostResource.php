9e96851c595ce5d3131fa95ef39511757a942e61
hudhaifa <halshmere44@gmail.com>
2024-08-12 17:17:39 +0300

    stast

---
+++ app/Filament/Resources/PostResource.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PostResource\Pages;
+use App\Filament\Resources\PostResource\RelationManagers;
+use App\Models\Post;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\Toggle;
+use Filament\Tables\Columns\IconColumn;
+
+class PostResource extends Resource
+{
+    protected static ?string $model = Post::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+        
+            ->schema([
+              Forms\Components\TextInput::make('titel')
+              ->required()
+              ->maxLength(255)
+            ->columnSpan('full'),
+              Forms\Components\Textarea::make('body')
+              ->required()
+              ->rows(3)
+              ->maxLength(255),
+
+            Forms\Components\Select::make('user_id')
+            ->relationship('user', 'name') 
+            ->searchable()
+            ->preload()
+            ->required(),
+            Forms\Components\Toggle::make('active')
+            ->required()
+            ->default(true)
+            ->label('نشيط'),
+
+
+            ]);
+            
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                  Tables\Columns\TextColumn::make('id') ->label("رقم"),
+                  Tables\Columns\TextColumn::make('titel') ->searchable()->label("العنوان"),
+                  Tables\Columns\TextColumn::make('body')->label("المحتوى "),
+                  Tables\Columns\TextColumn::make('user_id')->label("رقم المستخدم"),
+                IconColumn::make('active')
+                ->label('الحالة')
+                ->boolean()
+                ->trueIcon('heroicon-o-check-circle')
+                ->falseIcon('heroicon-o-x-circle')
+                ->trueColor('success')
+                ->falseColor('danger'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPosts::route('/'),
+            'create' => Pages\CreatePost::route('/create'),
+            'edit' => Pages\EditPost::route('/{record}/edit'),
+        ];
+    }
+}
